f513bb8ba26235e1fb97d4a7cdc38ce6e3cb5820
AlgaeCraft <142434200+AlgaeCraft@users.noreply.github.com>
2025-01-12 15:54:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🛠️Creating Pokemons", "➕Adding Event Pokemons"];
+const statusMessages = ["🛠️ Adding 999+ Atacks", "🔗 https://discord.gg/ktFDBgRpcY"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
